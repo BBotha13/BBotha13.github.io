9135b361384a4be9cdaa983cd0d89e9c02839efe
BBotha13 <108213269+BBotha13@users.noreply.github.com>
2022-08-04 17:08:11 +0200

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,31 @@
+//change navbar background color on scroll
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 50) {
+    nav.classList.add("active_nav");
+  } else {
+    nav.classList.remove("active_nav");
+  }
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-Menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+//shop category list
+
+const categoryBtn = document.querySelector("#category-container");
+const isCatActive = document.querySelector(".category-list ul");
+
+categoryBtn.addEventListener("mouseover", () => {
+  isCatActive.style.display = "block";
+});
+categoryBtn.addEventListener("mouseout", () => {
+  isCatActive.style.display = "none";
+});
